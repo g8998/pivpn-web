1c69efd803922e9138b5fb046e29e54637acdcf7
g8998 <135697447+g8998@users.noreply.github.com>
2023-07-03 19:49:24 +0200

    Update script.sh

--- app/script.sh
+++ app/script.sh
@@ -4,7 +4,6 @@ user=$1
 password=$2
 userPivpn=$(sudo cat /etc/pivpn/openvpn/setupVars.conf | grep install_user | sed s/install_user=//)
 
-#if echo "$password" | su -c "echo" "$user" >/dev/null 2>&1; then
 if echo "$password" | su -c "echo" "$user" >/dev/null 2>&1 && [ "$user" == "$userPivpn" ]; then
     echo "Authenticated"
 else
