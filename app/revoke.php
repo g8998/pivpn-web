7b5b9387b2e5e25f18f550810f3decca885fdd57
g8998 <135697447+g8998@users.noreply.github.com>
2023-07-03 20:05:30 +0200

    Update revoke.php

--- app/revoke.php
+++ app/revoke.php
@@ -1,22 +1,24 @@
 <?php
 require_once 'auth.php';
 ?>
+
 <?php
 $name = $_GET['name'];
 
 $output = shell_exec("pivpn -r -y $name");
 
 if ($output) {
-  ob_start();
-  include_once 'index.php';
-  $currentPath = rtrim(dirname($_SERVER['PHP_SELF']), '/');
-  header("Refresh: 0; URL=$currentPath/../index.php");
+    ob_start();
+    include_once 'index.php';
+    $currentPath = rtrim(dirname($_SERVER['PHP_SELF']), '/');
+    header("Refresh: 0; URL=$currentPath/../index.php");
 } else {
-  $logFilePath = '../logs/error.log';
-  $errorMessage = 'Error revoking client: The client '.$name.' does not exist';
-  file_put_contents($logFilePath, date('Y-m-d H:i:s') . ' - ' . $errorMessage . "\n", FILE_APPEND);
+    $logFilePath = '../logs/error.log';
+    $errorMessage = 'Error revoking client: The client '.$name.' does not exist';
+    file_put_contents($logFilePath, date('Y-m-d H:i:s') . ' - ' . $errorMessage . "\n", FILE_APPEND);
 
-  echo $errorMessage;
+    echo $errorMessage;
 }
+
 ob_end_clean();
 ?>
